f81096a6b2e5756c088db09c9fb08954c5c50ef6
Joost van Ophem <altumspatium@gmail.com>
2024-06-10 14:27:15 +0200

    removed cycling between images and interpolating images between.

--- src/main.py
+++ src/main.py
@@ -17,20 +17,12 @@ from utils import (
     compute_pairwise_distances,
     objective_function,
     compute_translations,
-    compute_translations,
-    sort_coordinates_with_others,
-    closest_points_to_line
+    compute_translations
 )
 
 
 
 ####################### VARIABLES #######################
-
-
-# Define the callback to update the clicked images
-CLICKED_POINTS = []
-CLICKED_POINTS_INDEX = 0
-PREV_CLICKDATA = None
 
 # Paths to cache files
 data_path = Path("data/")
@@ -128,9 +120,9 @@ app.layout = html.Div([
         ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'}),
         html.Div([
             html.Img(id='click-image', style={'height': '200px'}),
-            html.Div(id='click-index'),
-            html.Button('Reset', id='image-reset-button', n_clicks=0),
-            html.Button('Find between', id='image-find-between-button', n_clicks=0)
+            html.Div(id='click-index')
+            # html.Button('Reset', id='image-reset-button', n_clicks=0),
+            # html.Button('Find between', id='image-find-between-button', n_clicks=0)
         ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'}),
         dcc.Interval(
             id='interval-component',
@@ -286,57 +278,6 @@ def update_plot(n_clicks, current_fig):
     return current_fig, n_clicks
 
 
-@app.callback(
-    Output('image-find-between-button', 'n_clicks'),
-    Input('image-find-between-button', 'n_clicks')
-)
-def find_images_between_clicks(n_clicks):
-    if not n_clicks:
-        return None
-
-    global CLICKED_POINTS
-    global CLICKED_POINTS_INDEX
-
-    coords = []
-    for _, coord in CLICKED_POINTS:
-        coords.append(coord)
-
-    xs = np.array([coord[0] for coord in coords])
-    min_x = np.min(xs)
-    max_x = np.max(xs)
-    ys = np.array([coord[1] for coord in coords])
-    min_y = np.min(ys)
-    max_y = np.max(ys)
-
-    global fig
-    labels = []
-    imgs = []
-    x_coords = []
-    y_coords = []
-    for trace in fig.data:
-        for x_coord in trace.x:
-            x_coords.append(x_coord)
-        for y_coord in trace.y:
-            y_coords.append(y_coord)
-
-        for data in trace.customdata:
-            labels.append(data[0])
-            imgs.append(data[1])
-
-    all_coords = []
-    sublabels = []
-    subimgs = []
-    for i, (x, y) in enumerate(zip(x_coords, y_coords)):
-        if (x >= min_x) and (x <= max_x) and (y >= min_y) and (y <= max_y):
-            all_coords.append((x,y))
-            sublabels.append(labels[i])
-            subimgs.append(imgs[i])
-
-    sorted_coords, (sorted_imgs, sorted_labes) = sort_coordinates_with_others(all_coords, subimgs, sublabels)
-    CLICKED_POINTS = closest_points_to_line(sorted_coords, sorted_imgs, sorted_labes, 1)
-    CLICKED_POINTS_INDEX = 0
-    return None
-
 
 # Define the callback to update the image
 @callback(
@@ -355,61 +296,17 @@ def display_hover_image(hoverData):
 @callback(
     [Output('click-image', 'src'),
      Output('click-index', 'children')],
-    [Input('scatter-plot', 'clickData'),
-     Input('interval-component', 'n_intervals')]
+    [Input('scatter-plot', 'clickData')]
 )
-def display_click_image(clickData, n_intervals):
-    global CLICKED_POINTS # TODO: please don't use global variables, it's bad practice
-    global CLICKED_POINTS_INDEX # TODO: please don't use global variables, it's bad practice
-
-    if CLICKED_POINTS:
-        click_image, click_label = CLICKED_POINTS[CLICKED_POINTS_INDEX][0]
-
-        return click_image, click_label
-    return '', ''
-
-
-@callback(
-    [Input('scatter-plot', 'clickData'),
-     Input('interval-component', 'n_intervals')]
-)
-def cycle_through_clicked_points(clickData, n_intervals):
-    global CLICKED_POINTS # TODO: please don't use global variables, it's bad practice
-    global CLICKED_POINTS_INDEX # TODO: please don't use global variables, it's bad practice
-
-    if CLICKED_POINTS:
-        CLICKED_POINTS_INDEX = (CLICKED_POINTS_INDEX + 1) % len(CLICKED_POINTS)
-    return
-
-
-# Callback for gathering info on the actually clicked points
-@callback(
-    Output('image-reset-button', 'n_clicks'),
-    [Input('scatter-plot', 'clickData'),
-     Input('interval-component', 'n_intervals'),
-     Input('image-reset-button', 'n_clicks')]
-)
-def find_clicked_points(clickData, n_intervals, n_clicks):
-    global CLICKED_POINTS # TODO: please don't use global variables, it's bad practice
-    global CLICKED_POINTS_INDEX # TODO: please don't use global variables, it's bad practice
-    global PREV_CLICKDATA # TODO: please don't use global variables, it's bad practice
-
-    if n_clicks:
-        CLICKED_POINTS = []
-        CLICKED_POINTS_INDEX = 0
-        # Return an empty string to clear the image
-        return None
-
-
-    if (clickData is not None) and not (clickData == PREV_CLICKDATA):
+def display_click_image(clickData):
+    if clickData is not None:
         original_label = clickData['points'][0]['customdata'][0]
         original_image = clickData['points'][0]['customdata'][1]
         x_coord = clickData['points'][0]['x']
         y_coord = clickData['points'][0]['y']
-        CLICKED_POINTS.append([(original_image, f'Original Label: {original_label}'), (x_coord, y_coord)])
-        # reset clickData until you click on something again
-        PREV_CLICKDATA = clickData
-    return None
+
+        return original_image, f'Original Label: {original_label}'
+    return '', ''
 
 
 if __name__ == '__main__':
